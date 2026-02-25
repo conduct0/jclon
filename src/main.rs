4cf9650b24234c9b01fd27ed53022f843029a4da
conducto <43495400+conduct0@users.noreply.github.com>
2026-02-25 23:28:12 +0100

    refactor(rename): to jclon

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use jsonparse::Config;
+use jclon::Config;
 use std::env;
 use std::process;
 
@@ -8,7 +8,7 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = jsonparse::run(config) {
+    if let Err(e) = jclon::run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
